8140e48881dc0e722dd53e836b9a5fcb4c268418
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 16:34:09 +0300

    commit

--- index.js
+++ index.js
@@ -10,22 +10,18 @@ app.get("/", (req, res) => {
   res.send("welcome to express");
 });
 
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
   const { name, emial } = req.body;
-res.json({name, emial})
- 
-})
-app.put('/users/:id', (req, res) => {
-  const userId  = req.params.id;
+  res.json({ name, emial });
+});
+app.put("/users/:id", (req, res) => {
+  const userId = req.params.id;
   const { name, email } = req.body;
   // update user with given id
   res.json({
-    message: `update ${name}, ${email} and ${userId}`
-  })
-
- 
- 
-})
+    message: `update ${name}, ${email} and ${userId}`,
+  });
+});
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
