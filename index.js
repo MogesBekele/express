50c530c241fbb61123a4c1ca11901b01d191d2b4
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 16:13:26 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import express from 'express';
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+
+  res.send('welcome to express')
+
+})
+
+app.listen(PORT, (req, res)=>{
+  console.log(`Server is running on port ${PORT}`)
+})
