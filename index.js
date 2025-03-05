7dd067a154df663479ad57a17d0155e3b3198d4c
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 16:19:14 +0300

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
@@ -6,12 +6,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
+  res.send("welcome to express");
+});
 
-  res.send('welcome to express')
-
+app.post('/users/:name/:id', (req, res) => {
+  const { name, id } = req.params;
+  res.send(`wellcome ${name} ${id}`);
+ 
 })
 
-app.listen(PORT, (req, res)=>{
-  console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
