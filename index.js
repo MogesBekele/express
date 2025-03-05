cc8c494d6a30528f50432931b6c6aa2057e58224
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 16:42:29 +0300

    commit

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("welcome to express");
+app.get("/user/:name/:id", (req, res) => {
+  const { name, id } = req.params;
+
+ res.json({  name,  id})
 });
 
 app.post("/users", (req, res) => {
@@ -29,8 +31,7 @@ app.delete("/users/:id", (req, res) => {
   res.json({
     message: `delete user with id ${userId}`,
   });
-
-})
+});
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
