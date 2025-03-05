c10a56dbe580b5212278390b6d17612ec6c08d7f
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 16:53:25 +0300

    commit

--- index.js
+++ index.js
@@ -32,14 +32,13 @@ app.delete("/users/:id", (req, res) => {
   });
 });
 
-
 //catch all invalid requests
 
-app.get('*',(req, res) => {
-  res.send('invalid request');
+app.get("*", (req, res) => {
+  res.send("invalid request");
 });
 
- // Start the server  on the specified port
+// Start the server  on the specified port
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}`);
 });
