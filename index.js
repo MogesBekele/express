0a3889cf0eae8c5af3944a86ebee743e5e0e0ce1
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 20:00:48 +0300

    commit

--- index.js
+++ index.js
@@ -11,32 +11,7 @@ app.get("/user/:name/:id([0-9]{4})", (req, res) => {
   res.json({ name, id });
 });
 
-app.post("/users", (req, res) => {
-  const { name, email } = req.body;
-  res.json({ name, email });
-});
-app.put("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  const { name, email } = req.body;
-  // update user with given id
-  res.json({
-    message: `update ${name}, ${email} and ${userId}`,
-  });
-});
 
-app.delete("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  // delete user with given id
-  res.json({
-    message: `delete user with id ${userId}`,
-  });
-});
-
-//catch all invalid requests
-
-app.get("*", (req, res) => {
-  res.send("invalid request");
-});
 
 // Start the server  on the specified port
 app.listen(PORT, (req, res) => {
