874fd22375e7066dd7a6fb877f0a32c6cbb8d841
MogesBekele <mogesbekele32@gmail.com>
2025-03-05 20:09:26 +0300

    commit

--- index.js
+++ index.js
@@ -6,20 +6,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
-
 //middleware
 
 app.use((req, res, next) => {
-
-  console.log(`a new requesr resieved at ` + Date.now())
+  console.log(`a new requesr resieved at ` + Date.now());
   next();
-})
-
-app.get("/", (req, res) => {
-res.send('Welcome')
 });
 
-
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
 // Start the server  on the specified port
 app.listen(PORT, (req, res) => {
