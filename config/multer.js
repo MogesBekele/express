014ef39d0539e031ff1591fd49949e5d60ea3c6c
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 18:07:05 +0300

    commit

--- config/multer.js
+++ config/multer.js
@@ -1,2 +1,10 @@
 import express from 'express';
 import multer from 'multer';
+
+
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
