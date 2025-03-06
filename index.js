f2d3d65e2d2bd68de08c04f26184df140484cbed
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 21:17:52 +0300

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const app = express();
 
 const MONGODB_URI =
- 'mongodb+srv://mogesbekele32:<db_password>@cluster0.xcdr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+ 'mongodb+srv://mogesbekele32:moges123@cluster0.xcdr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 //
 
