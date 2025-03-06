9619480ccd044c9862d936d65898e5d4d6262377
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 17:28:03 +0300

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user", route);
 
