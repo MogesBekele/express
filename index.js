ec307f1f8fa29e56d846421983ce196c7c17a202
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 22:03:33 +0300

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const PORT = 3000;
 
-const MONGODB_URI = 'mongodb+srv://mogesbekele32:moges123@cluster0.xcdr3.mongodb.net/express'
+const MONGODB_URI = 'mongodb+srv://mogesbekele32:moges123@cluster0.xcdr3.mongodb.net/exp'
 
 mongoose.connect(MONGODB_URI).then(() => {
   console.log("Connected to MongoDB");
