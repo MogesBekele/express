a144fa7169121185e7233e5d583fa3caddf7fbd4
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 21:06:42 +0300

    commit

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 import express from "express";
 import route from "./routes/route.js";
-import multer from "multer";
-import { storage } from "./config/multer.js";
 
 const app = express();
 
-const upload = multer({
-  storage,
-  limits: { fileSize: 1000000 }, // 1MB
-});
+MONGODB_URI = "mongodb+srv://mogesbekele32:moges123@cluster0.xcdr3.mongodb.net";
+
+//mongodb+srv://mogesbekele32:<db_password>@cluster0.xcdr3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/user", route);
 
 app.post("/form", (req, res) => {
   console.log(req.body);
-  console.log(req.file); // req.file contains the uploaded file object
 
   res.send("form received");
 });
