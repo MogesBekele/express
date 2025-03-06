704b19dcea3dc449b7d5b4711d372addcb531e9b
MogesBekele <mogesbekele32@gmail.com>
2025-03-06 17:45:46 +0300

    commit

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ import multer from "multer";
 
 const app = express();
 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+})
 const PORT = 3000;
 const upload = multer();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
