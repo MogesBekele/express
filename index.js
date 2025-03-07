274fde7ca1ccb240d356b14ba92c8a0af6dd88de
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 10:04:26 +0300

    coomit

--- index.js
+++ index.js
@@ -13,10 +13,7 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/remove-cookie", (req, res) => {
-  res.clearCookie("name");
-  res.send("Cookie removed");
-});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
