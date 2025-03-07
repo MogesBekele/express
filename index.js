fde4cb090715a187fa6e5b01c1e7f1de175ae625
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 21:05:38 +0300

    commit

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import session from "express-session";
 
-
 const PORT = 3000;
 const app = express();
 app.use(cookieParser());
-app.use(session( {
-  secret: "mysecretkey",
-  resave: false,
-  saveUninitialized: false,
-  //cookie: { secure: false }  // Set secure to false for development environment. In production, set secure to true.  // This will make the cookie accessible only over HTTP (not HTTPS)
-}))
-
-
-
-
-
+app.use(
+  session({
+    secret: "mysecretkey",
+    resave: false,
+    saveUninitialized: false,
+    //cookie: { secure: false }  // Set secure to false for development environment. In production, set secure to true.  // This will make the cookie accessible only over HTTP (not HTTPS)
+  })
+);
 
 app.get("/", (req, res) => {
- 
   res.send("Hello, World!");
 });
+const users = []
+app.post('/register', (req, res) => {
+  const {username, password} =req.body;
+  users.push({username, password})
+  
 
 
-
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
