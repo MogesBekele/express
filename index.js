c620892a5875d84e1a79c3fcbe9ec8702f1b60f6
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 10:07:12 +0300

    commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
+ 
   res.send("Hello, World!");
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
