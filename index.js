d4affcc3c769b5880600b8db8fa26887a8f40416
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 09:42:44 +0300

    commit

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
   // Set a cookie
-  res.cookie("name", "express-app", {maxAge: 360000});
+  res.cookie("name", "express-app", { maxAge: 360000 });
   res.send("Hello, World!");
 });
 
 app.get("/fetch", (req, res) => {
   // Get the cookie
-  console.log(req.cookies)
+  console.log(req.cookies);
   res.send("Cookie value: " + req.cookies.myCookie);
+});
+
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("myCookie");
+  res.send("Cookie removed");
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
