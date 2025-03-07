399f58d77c65477e658d5740961136421a5fc1e0
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 09:07:20 +0300

    commit

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 
-
-
 const app = express();
-app.use(cookieParser())
-
-
-
+app.use(cookieParser());
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+  // Cookies that have not been signed
+  console.log("Cookies: ", req.cookies);
 
-app.get('/', (req,res)=>{
-  res.send('Hello, World!')
-   // Cookies that have not been signed
-   console.log('Cookies: ', req.cookies)
-
-   // Cookies that have been signed
-   console.log('Signed Cookies: ', req.signedCookies)
-})
-
-
-
-
-
+  // Cookies that have been signed
+  console.log("Signed Cookies: ", req.signedCookies);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
