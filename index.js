2705c62e72a0b471a1b2c34c925e24e90942bb0b
MogesBekele <mogesbekele32@gmail.com>
2025-03-07 08:56:30 +0300

    commit

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import express from "express";
-import route from "./routes/route.js";
+
 
 
 const app = express();
+app.use(express.json());
 
 
 
 const PORT = 3000;
 
 
-app.use("/user", route);
+app.get('/', (req,res)=>{
+  res.send('Hello, World!')
+})
 
-app.post("/form", (req, res) => {
-  console.log(req.body);
 
-  res.send("form received");
-});
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
