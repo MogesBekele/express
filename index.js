454a5e3052940eef0b788606491029f09a5f8907
MogesBekele <mogesbekele32@gmail.com>
2025-03-08 14:40:46 +0300

    commit

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.get("/", (req, res) => {
 });
 
 app.get('/api/products', (req, res) => {
-  
+  const products = [
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 },
+  ];
+
 })
 
 app.listen(PORT, () => {
